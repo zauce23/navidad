a997b02181b1f105b05ae3d71f705f07b2b005a8
zauce23 <anthonyzairpalacios@gmail.com>
2025-11-02 16:19:33 -0500

    Add files via upload

---
+++ jojojojo.php
@@ -0,0 +1,10 @@
+
+<?php
+    $respuestas = array(
+        "regalo, felicidades te has portado muy bien",
+        "carbon, que pena te has portado muy mal"
+        );
+    $random = array_rand($respuestas);
+    echo $respuestas[$random];
+    ?>
+                            
